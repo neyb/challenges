29cc451e872eb6dbf0c17bf305af04b5f708e15e
neyb <noo.blaster@gmail.com>
2021-12-18 10:30:53 +0100

    day16 part 2

--- kotlin/advent-of-code/src/aoc2021/16.kt
+++ kotlin/advent-of-code/src/aoc2021/16.kt
@@ -1,22 +1,30 @@
 package aoc2021.day16
 
 import common.*
+import kotlin.math.max
+import kotlin.math.min
 
-fun main() = day(2021, 16, part1)
+fun main() = day(2021, 16, part1, part2)
 
 val part1 = { lines: List<String> ->
-
-
-
-    val packetReader = PacketReader(lines.asSequence()
-                                        .flatMap { it.asSequence() }
-                                        .flatMap { it.digitToInt(16).toString(2).padStart(4,'0').asSequence().map { it.digitToInt() } }
-                                   )
-
-    packetReader.readAllPackets().asSequence()
+    createPacketReader(lines).readAllPackets().asSequence()
         .flatMap { it.allPackets() }
         .fold(0) { versionSum, packet -> versionSum + packet.version }
 }
+
+val part2 = { lines: List<String> ->
+    createPacketReader(lines).readAllPackets().asSequence()
+        .fold(0L) { versionSum, packet -> versionSum + packet.value() }
+}
+
+private fun createPacketReader(lines: List<String>) = PacketReader(lines.asSequence()
+                                                                 .flatMap { it.asSequence() }
+                                                                 .flatMap {
+                                                                     it.digitToInt(16).toString(2).padStart(4, '0')
+                                                                         .asSequence()
+                                                                         .map { it.digitToInt() }
+                                                                 }
+                                                                  )
 
 typealias Version = Short
 
@@ -24,11 +32,11 @@ sealed interface Packet {
     val version: Version
     val type: Short
     val subPackets: List<Packet>
+    fun value(): Long
 
     fun allPackets(): Collection<Packet> = listOf(this) + subPackets.flatMap { it.allPackets() }
 }
 
-@JvmRecord
 data class LiteralPacket(override val version: Version, val value: Long) : Packet {
     companion object {
         val type = 4.toShort()
@@ -37,15 +45,27 @@ data class LiteralPacket(override val version: Version, val value: Long) : Packe
 
     override val type get() = Companion.type
     override val subPackets get() = Companion.subPackets
-
+    override fun value() = value
 }
 
-@JvmRecord
 data class OperatorPacket(
     override val version: Version,
     override val type: Short,
     override val subPackets: List<Packet>
-                         ) : Packet
+                         ) : Packet {
+    fun subValues() = subPackets.asSequence().map { it.value() }
+
+    override fun value() = when (type.toInt()) {
+        0 -> subValues().fold(0L, Long::plus)
+        1 -> subValues().fold(1, Long::times)
+        2 -> subValues().reduce(::min)
+        3 -> subValues().reduce(::max)
+        5 -> subPackets.map { it.value() }.let { (a,b) -> if(a > b) 1 else 0 }
+        6 -> subPackets.map { it.value() }.let { (a,b) -> if(a < b) 1 else 0 }
+        7 -> subPackets.map { it.value() }.let { (a,b) -> if(a == b) 1 else 0 }
+        else -> throw Exception("unsupported type : $type")
+    }
+}
 
 class PacketReader(input: Sequence<Int>) {
     private val iterator = input.iterator()
