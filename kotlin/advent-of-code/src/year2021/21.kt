fdc932ea29fa794db725bfb236a347135210879a
neyb <noo.blaster@gmail.com>
2021-12-25 17:46:21 +0100

    day 21

---
+++ kotlin/advent-of-code/src/year2021/21.kt
@@ -0,0 +1,74 @@
+package year2021.day21
+
+import common.*
+
+fun main() = day(2021, 21, part1, part2) {
+    val lines = readLines()
+    lines[0].last().digitToInt() to lines[1].last().digitToInt()
+}
+
+typealias Input = Pair<Int, Int>
+
+val part1 = { (p1position, p2position): Input ->
+    val game = Game(listOf(Player(p1position - 1), Player(p2position - 1)), 1000)
+    var nbDiceRolled = 0
+    val dice = generateSequence(0) { (it + 1) % 100 }.map { it + 1 }.onEach { nbDiceRolled += 1 }.iterator()
+
+    val finalGame = generateSequence(game) {
+        it.play(dice.next() + dice.next() + dice.next())
+    }.first { it.isOver() }
+
+    finalGame.players.minOf { it.score } * nbDiceRolled
+}
+
+val part2 = { (p1position, p2position): Input ->
+    val game = Game(listOf(Player(p1position - 1), Player(p2position - 1)), 21)
+
+    val diceSumsProb = (1..3).flatMap { a -> (1..3).flatMap { b -> (1..3).map { c -> a + b + c } } }
+        .groupingBy { it }
+        .eachCount()
+
+    val quantumGames = mutableMapOf<Game, Long>()
+
+    val winProbs = mutableListOf(0L, 0L)
+
+    generateSequence(QuantumGame(game, 1)) {
+        quantumGames
+            .minByOrNull { it.key.players.minOf { it.score } }
+            ?.also { quantumGames.remove(it.key) }
+            ?.let { (game, prob) -> QuantumGame(game, prob) }
+    }
+        .forEach { currentQg ->
+            if (currentQg.game.isOver()) currentQg.game.indexOfWinner().let { playerIndex ->
+                winProbs[playerIndex] = winProbs[playerIndex] + currentQg.prob
+            } else diceSumsProb
+                .map { (diceSum, prob) -> currentQg.play(diceSum, prob.toLong()) }
+                .forEach { resultingQg -> quantumGames.merge(resultingQg.game, resultingQg.prob, Long::plus) }
+        }
+
+    winProbs.maxOf { it }
+}
+
+data class QuantumGame(val game: Game, val prob: Long) {
+    fun play(diceSum: Int, prob: Long) = QuantumGame(game.play(diceSum), this.prob * prob)
+}
+
+data class Game(val players: List<Player>, val winScore: Int, val nextPlayerIndex: Int = 0) {
+    fun play(diceSum: Int) = Game(
+        players.mapIndexed { index, player -> if (index == nextPlayerIndex) player.play(diceSum) else player },
+        winScore,
+        (nextPlayerIndex + 1) % players.size,
+                                 )
+
+    fun isOver() = indexOfWinner() >= 0
+    fun indexOfWinner() = players.indexOfFirst { it.score >= winScore }
+}
+
+
+data class Player(val position: Int, val score: Int = 0) {
+    fun play(diceSum: Int) = ((position + diceSum) % 10).let { newPosition ->
+        Player(newPosition, score + newPosition + 1)
+    }
+}
+
+
