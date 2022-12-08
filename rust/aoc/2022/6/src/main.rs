d00179d0926b2f72e03d5f7310dfc8c3f13bac73
neyb <noo.blaster@gmail.com>
2022-12-08 08:49:19 +0100

    aoc 2022: day 6

---
+++ rust/aoc/2022/6/src/main.rs
@@ -0,0 +1,25 @@
+use itertools::Itertools;
+
+fn main() {
+    let first_line = challenges_common::get_input_lines(vec!["aoc", "2022", "6.txt"])
+        .next()
+        .unwrap();
+
+    println!("part1 {}", first_line.first_marker_position(4));
+    println!("part1 {}", first_line.first_marker_position(14));
+}
+
+trait Packet {
+    fn first_marker_position(&self, marker_size: usize) -> usize;
+}
+
+impl Packet for String {
+    fn first_marker_position(&self, marker_size: usize) -> usize {
+        self.chars()
+            .collect_vec()
+            .windows(marker_size)
+            .position(|window| window.iter().all_unique())
+            .unwrap()
+            + marker_size
+    }
+}
