fc19dab68def620291fb3304ed7561cba2025ba2
neyb <noo.blaster@gmail.com>
2024-04-09 16:59:59 +0200

    fix new.sh

--- rust/aoc/new.sh
+++ rust/aoc/new.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cargo new "aoc_$1_$2"
-mkdir $1
-mv "aoc_$1_$2" $1/$2
+cargo new --name "aoc_$1_$2" "$1/$2"
 cd $1/$2
-cargo add challenges_common
-cargo add anyhow
+cargo add challenges_common anyhow
