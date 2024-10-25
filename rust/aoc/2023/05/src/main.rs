3e856fd7d241fd9f996bc10e0036fbd78ff250bb
neyb <noo.blaster@gmail.com>
2024-10-25 16:13:32 +0200

    part 1

--- rust/aoc/2023/05/src/main.rs
+++ rust/aoc/2023/05/src/main.rs
@@ -1,15 +1,24 @@
 use std::ops::Range;
-use std::result;
 use std::str::FromStr;
 
 use anyhow::{anyhow, Result};
-
 use challenges_common::MyIterTools;
 use itertools::Itertools;
-use lazy_regex::{lazy_regex, regex};
 
 fn main() {
-    println!("Hello, world!");
+    let content = challenges_common::get_input_content(&["aoc", "2023", "05.txt"]);
+    println!("part 1: {}", part1(&content));
+}
+
+type Position = u64;
+
+fn part1(content: &str)-> Position {
+    let almanac = content.parse().unwrap();
+    get_min_location(&almanac)
+}
+
+fn get_min_location(almanac: &Almanac) -> Position {
+    almanac.locations().min().unwrap()
 }
 
 struct Almanac {
@@ -17,17 +26,14 @@ struct Almanac {
     mappings: Vec<Mapping>,
 }
 
-struct Category {
-    numbers: Vec<u32>,
-}
-
-struct Mapping {
-    transformations: Vec<Transformation>,
-}
-
-struct Transformation {
-    source_range: Range<u32>,
-    transformation: u32,
+impl Almanac {
+    fn locations(&self) -> impl Iterator<Item =Position> + '_ {
+        self.seeds.numbers.iter().map(move |seed| {
+            self.mappings
+                .iter()
+                .fold(*seed, |value, mapping| mapping.map(value))
+        })
+    }
 }
 
 impl FromStr for Almanac {
@@ -41,26 +47,17 @@ impl FromStr for Almanac {
             .ok_or_else(|| anyhow!("cannot get seeds group"))?
             .get(0)
             .ok_or_else(|| anyhow!("cannot get seeds line"))?
-            .parse()?            ;
+            .parse()?;
 
-        let mappings = group_iterator
-            .map(|lines| lines.iter().skip(1).map(|line| line.parse()))
-            .collect();
+        let mappings = group_iterator.map(|lines| lines.try_into()).try_collect()?;
 
-        Ok(Self{seeds,mappings})
+        Ok(Self { seeds, mappings })
     }
 }
 
-impl FromStr for Mapping{
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self> {
-        let (_, [source,_,_]) = lazy_regex::regex_captures!(r"(\d+) (\d+) (\d+)", s).ok_or_else(||anyhow!("cannot extract Mapping from {}", s))?;
-
-        let transformations =
-
-        Ok(Self{transformations})
-    }
+#[derive(Debug, PartialEq)]
+struct Category {
+    numbers: Vec<Position>,
 }
 
 impl FromStr for Category {
@@ -72,10 +69,131 @@ impl FromStr for Category {
             .captures_iter(s)
             .map(|capt| {
                 let (number_str, []) = capt.extract();
-                number_str.parse::<u32>().into()
+                number_str.parse::<Position>()
             })
-            .collect::<Result<_>>()?;
+            .try_collect()?;
 
         Ok(Self { numbers })
+    }
+}
+
+struct Mapping {
+    transformations: Vec<Transformation>,
+}
+
+impl Mapping {
+    fn map(&self, value: Position) -> Position {
+        match self
+            .transformations
+            .iter()
+            .find_or_first(|transformation| transformation.concerns(value))
+        {
+            Some(transformation) => transformation.map(value),
+            None => value,
+        }
+    }
+}
+
+impl<S> TryFrom<Vec<S>> for Mapping
+where
+    S: AsRef<str>,
+{
+    type Error = anyhow::Error;
+
+    fn try_from(lines: Vec<S>) -> Result<Self> {
+        let transformations = lines
+            .iter()
+            .skip(1)
+            .map(|line| line.as_ref().parse::<Transformation>())
+            .try_collect()?;
+
+        Ok(Self { transformations })
+    }
+}
+
+impl FromStr for Mapping {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let transformations = s.lines().skip(1).map(|line| line.parse()).try_collect()?;
+        Ok(Self { transformations })
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Transformation {
+    source_range: Range<Position>,
+    transformation: i64,
+}
+
+impl FromStr for Transformation {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let elements = s.split(' ').collect_vec();
+        let [destination_start, source_start, source_range] = elements.as_slice() else {
+            anyhow::bail!("cannot parse transformation {}", s)
+        };
+
+        let source_start: u64 = source_start.parse()?;
+        let source_range: u64 = source_range.parse()?;
+        let destination_start: u64 = destination_start.parse()?;
+
+        Ok(Self::new(
+            source_start..source_start + source_range,
+            destination_start as i64 - source_start as i64,
+        ))
+    }
+}
+
+impl Transformation {
+    fn new(source_range: Range<Position>, transformation: i64) -> Self {
+        Self {
+            source_range,
+            transformation,
+        }
+    }
+
+    fn concerns(&self, value: Position) -> bool {
+        self.source_range.contains(&value)
+    }
+
+    fn map(&self, value: Position) -> Position {
+        if self.source_range.contains(&value) {
+            (value as i64 + self.transformation) as Position
+        } else {
+            value
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parsing_a_category() {
+        let category = "seeds: 79 14 55 13";
+        let category = Category::from_str(category).unwrap();
+        assert_eq!(
+            category,
+            Category {
+                numbers: vec![79, 14, 55, 13]
+            }
+        );
+    }
+
+    #[test]
+    fn parsing_a_transformation() {
+        let transformation = "50 98 2";
+        let transformation = Transformation::from_str(transformation).unwrap();
+        assert_eq!(transformation, Transformation::new(98..100, -48));
+    }
+
+    #[test]
+    fn part1_given_test() {
+        let almanac = challenges_common::get_input_content(&["aoc", "2023", "05-test.txt"]);
+        let almanac = Almanac::from_str(&almanac).unwrap();
+        assert_eq!(almanac.locations().min(), Some(35));
     }
 }
