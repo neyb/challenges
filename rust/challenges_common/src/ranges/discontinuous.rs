d72ee8da20b76c324fdacd9d3a45f1f45d774808
neyb <noo.blaster@gmail.com>
2024-10-26 15:34:23 +0200

    wip

--- rust/challenges_common/src/ranges/discontinuous.rs
+++ rust/challenges_common/src/ranges/discontinuous.rs
@@ -1,23 +1,65 @@
-struct Range<P>
-where
-    P: PartialOrd + Stepable,
-{
+use crate::ranges::{JoinedResult, WithoutResult};
+
+pub enum Range<P> {
+    Empty,
+    NotEmpty(NotEmptyRange<P>),
+}
+
+struct NotEmptyRange<P> {
     start: P,
     end: P,
 }
 
-impl<P> Range<P>
+impl<P> super::Range for NotEmptyRange<P>
 where
-    P: PartialOrd + Stepable,
+    P: Ord + Stepable + Copy,
 {
-    fn contains(&self, point: &P) -> bool {
-        self.start <= *point && *point < self.end
+    type Element = P;
+
+    fn start(&self) -> Self::Element {
+        self.start
+    }
+
+    fn end(&self) -> Self::Element {
+        self.end
+    }
+
+    fn contains(&self, point: Self::Element) -> bool {
+        self.start <= point && point <= self.end
+    }
+
+    fn overlap(&self, other: &Self) -> bool {
+        self.contains(other.start)
+            || self.contains(other.end)
+            || other.contains(self.start)
+            || other.contains(self.end)
+    }
+
+    fn intersection(&self, other: &Self) -> Option<Self> {
+        todo!()
+    }
+
+    fn join(&self, other: &Self) -> JoinedResult<Self> {
+        todo!()
+    }
+
+    fn without(&self, other: &Self) -> WithoutResult<Self> {
+        todo!()
     }
 }
 
-impl<P> Range<P>
+impl<P> NotEmptyRange<P>
 where
-    P: PartialOrd + Copy + Stepable + std::ops::Sub<Output = P>,
+    P: Ord + Copy + Stepable,
+{
+    pub fn new(start: P, end: P) -> Self {
+        Self { start, end }
+    }
+}
+
+impl<P> NotEmptyRange<P>
+where
+    P: Ord + Copy + Stepable + std::ops::Sub<Output = P>,
 {
     fn len(&self) -> P {
         self.end - self.start
@@ -28,3 +70,21 @@ pub trait Stepable {
     fn next(&self) -> Self;
     fn prev(&self) -> Self;
 }
+
+macro_rules! impl_stepable {
+    ($($t:ty),*) => {
+        $(
+            impl Stepable for $t {
+                fn next(&self) -> Self {
+                    self + 1
+                }
+
+                fn prev(&self) -> Self {
+                    self - 1
+                }
+            }
+        )*
+    };
+}
+
+impl_stepable!(i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize);
--- rust/challenges_common/src/ranges/mod.rs
+++ rust/challenges_common/src/ranges/mod.rs
@@ -1,42 +1,132 @@
+use itertools::Either;
+use std::fmt::Debug;
+
 mod continuous;
 mod discontinuous;
 
-struct Ranges<P>
-where
-    P: PartialOrd + Copy + discontinuous::Stepable,
-{
-    ranges: Vec<discontinuous::Range<P>>,
+pub struct Ranges<R: Range> {
+    ranges: Vec<R>,
 }
 
-impl<P> Ranges<P>
-where
-    P: PartialOrd + Copy + discontinuous::Stepable,
-{
-    fn remove(&mut self, range: discontinuous::Range<P>) {
-        self.ranges = self
-            .ranges
-            .iter()
-            .flat_map(|current_range| {
-                match (
-                    current_range.contains(&range.start),
-                    current_range.contains(&range.end),
-                ) {
-                    (true, true) => vec![
-                        current_range.start..range.end,
-                        range.start + 1..current_range.end,
-                    ],
-                    (true, false) => vec![crate::ranges::discontinuous::Range {
-                        start: range.end,
-                        end: current_range.end,
-                    }],
-                    (false, true) => vec![crate::ranges::discontinuous::Range {
-                        start: current_range.start,
-                        end: range.start,
-                    }],
-                    (false, false) => vec![current_range.clone()],
+impl<R: Range> Ranges<R> {
+    fn empty() -> Self {
+        Self { ranges: Vec::new() }
+    }
+
+    fn new(ranges: Vec<R>) -> Self {
+        let mut result = Self { ranges };
+        result.simplify();
+        result
+    }
+
+    fn remove_ranges(&mut self, ranges: &Ranges<R>) -> Self {
+        let mut all_removed = Self::empty();
+        for range in ranges.ranges.iter() {
+            let removed = self.remove_range(range);
+            all_removed.ranges.extend(removed.ranges);
+        }
+        all_removed
+    }
+
+    fn remove_range(&mut self, range: &R) -> Self {
+        let mut all_removed = Self { ranges: Vec::new() };
+
+        let previous_ranges = std::mem::take(&mut self.ranges);
+        for previous_range in previous_ranges {
+            let WithoutResult { removed, remaining } = previous_range.without(range);
+            if let Some(removed) = removed {
+                all_removed.ranges.push(removed);
+            }
+            match remaining {
+                Remaining::Joined(range) => self.ranges.push(range),
+                Remaining::Splitted(first, second) => {
+                    self.ranges.push(first);
+                    self.ranges.push(second);
                 }
-            })
-            .filter(|r| !r.contains(&range.start) && !r.contains(&range.end))
-            .collect();
+            }
+        }
+        self.simplify();
+        all_removed.simplify();
+
+        all_removed
+    }
+
+    fn simplify(&mut self) {
+        self.ranges.sort_by_key(|r| (r.start(), r.end()));
+        let mut current = None;
+
+        let ranges = std::mem::take(&mut self.ranges);
+
+        for range in ranges {
+            match &current {
+                None => {
+                    current = Some(range);
+                }
+                Some(current_range) => match current_range.join(&range) {
+                    JoinedResult::Joined(joined) => {
+                        current = Some(joined);
+                    }
+                    JoinedResult::Disjoint => {
+                        self.ranges.push(current.replace(range).unwrap());
+                    }
+                },
+            };
+        }
+
+        if let Some(current) = current {
+            self.ranges.push(current);
+        }
+    }
+}
+
+trait Range: Sized {
+    type Element: Ord + Copy;
+
+    fn start(&self) -> Self::Element;
+    fn end(&self) -> Self::Element;
+
+    fn contains(&self, element: Self::Element) -> bool;
+
+    fn overlap(&self, other: &Self) -> bool;
+
+    fn intersection(&self, other: &Self) -> Option<Self>;
+    fn join(&self, other: &Self) -> JoinedResult<Self>;
+    fn without(&self, other: &Self) -> WithoutResult<Self>;
+}
+
+enum JoinedResult<R> {
+    Joined(R),
+    Disjoint,
+}
+
+struct WithoutResult<R> {
+    remaining: Remaining<R>,
+    removed: Option<R>,
+}
+
+enum Remaining<R> {
+    Joined(R),
+    Splitted(R, R),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod discontinuous {
+        use super::super::*;
+
+        #[test]
+        fn empty_ranges_should_be_empty() {
+            let ranges:Ranges<discontinuous::NotEmptyRange<i32>> = Ranges::empty();
+            assert_eq!(ranges.ranges.len(), 0);
+        }
+
+        #[test]
+        fn can_create_ranges() {
+            let range = discontinuous::NotEmptyRange::new(1, 2);
+            let mut ranges = Ranges::new(vec![range]);
+            assert_eq!(ranges.ranges.len(), 1);
+        }
     }
 }
