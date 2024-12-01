16a9b76068484576c7df0398c88b9da91277d2a5
neyb <noo.blaster@gmail.com>
2024-12-01 11:16:20 +0100

    template

---
+++ rust/aoc/new.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cargo new --name "aoc_$1_$2" "$1/$2"
+cd $1/$2
+cargo add challenges_common anyhow itertools lazy-regex
+cp -r ../../../template/aoc/* ./
---
+++ rust/template/aoc/src/part2.rs
@@ -0,0 +1,6 @@
+use anyhow::*;
+
+type Res = usize;
+pub(crate) fn run(_content: &String) -> Result<Res> {
+    todo!()
+}
