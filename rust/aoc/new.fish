a53ec5ec53e0ee391eb0bb1e9ab515feb5840c10
neyb <noo.blaster@gmail.com>
2024-12-01 16:07:46 +0100

    move template to aoc

--- rust/aoc/new.fish
+++ rust/aoc/new.fish
@@ -5,17 +5,17 @@ or return 1
 
 set -x YEAR $argv[1]
 set -x DAY $argv[2]
-set AOC_FOLDER ./(dirname (status filename))
-set TARGET_FOLDER "$AOC_FOLDER/$YEAR/$DAY"
+set AOC_FOLDER (realpath (dirname (status filename)))
+set TARGET_FOLDER "$AOC_FOLDER/$YEAR/$DAY/"
 
 echo "Creating new day $DAY for year $YEAR in $TARGET_FOLDER"
+read -P "Press enter to continue"
 
 mkdir -p $TARGET_FOLDER
 
-cp -r $AOC_FOLDER/../template/aoc/* $TARGET_FOLDER
+cp -r $AOC_FOLDER/template/* $TARGET_FOLDER
 
 for file in (find $TARGET_FOLDER -type f)
-    echo $file
     envsubst <$file >$file.tmp
     mv $file.tmp $file
 end
