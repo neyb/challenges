cda268cd1bd037e207563a964ab5eaf333d42a41
neyb <noo.blaster@gmail.com>
2024-12-02 22:16:38 +0100

    faster... but still not fast enough

--- rust/aoc/2023/21/src/part2.rs
+++ rust/aoc/2023/21/src/part2.rs
@@ -2,77 +2,68 @@ use crate::PositionType;
 use anyhow::*;
 use challenges_common::graph;
 use challenges_common::graph::{CannotParseGrid, Grid};
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::str::FromStr;
 
-type Res = usize;
-pub(crate) fn run(content: &String) -> Result<Res> {
-    let mut map: InfiniteMap = content.parse()?;
-    map.explore(26501365)?;
-    Ok(map.count_explored(26501365))
+type Res = u32;
+pub(crate) fn run(content: &String, steps: u32) -> Result<Res> {
+    let map: InfiniteMap = content.parse()?;
+
+    let start = map.start().ok_or_else(|| anyhow!("no start"))?;
+    let mut old_explored = HashSet::new();
+    let mut previous_explored = HashSet::new();
+    previous_explored.insert(start);
+
+    let mut count = (steps + 1) % 2;
+
+    for i_step in 0..steps {
+        let mut discovered = HashSet::new();
+
+        for coord in &previous_explored {
+            for neighbour in coord.neighbours(false) {
+                if map.get_type(neighbour) == PositionType::Empty
+                    && !old_explored.contains(&neighbour)
+                {
+                    if (i_step + 1) % 2 == steps % 2 && !discovered.contains(&neighbour) {
+                        count += 1;
+                    }
+                    discovered.insert(neighbour);
+                }
+            }
+        }
+
+        old_explored = previous_explored;
+        previous_explored = discovered;
+    }
+
+    Ok(count)
 }
 
 type Coord = graph::Coord<isize>;
-type Position = crate::Position<u32>;
-
 struct InfiniteMap {
     grid: Grid<PositionType, isize>,
-    positions: HashMap<Coord, Position>,
 }
 
 impl InfiniteMap {
-    fn explore(&mut self, times: u32) -> Result<()> {
-        let mut current_coords = vec![self.start().ok_or_else(|| anyhow!("no start"))?];
-
-        for i in 0..times {
-            let mut next_positions = Vec::new();
-            for current_coord in current_coords {
-                for next_coord in current_coord.neighbours(false) {
-                    let next_position = self.get_mut(next_coord);
-                    if next_position.should_be_explored() {
-                        next_position.explore_range = Some(i + 1);
-                        next_positions.push(next_coord);
-                    }
-                }
-            }
-
-            current_coords = next_positions;
-        }
-
-        anyhow::Ok(())
-    }
-
     fn start(&self) -> Option<Coord> {
         self.grid.find(|position| position == &PositionType::Start)
     }
 
-    fn get_mut(&mut self, coord: Coord) -> &mut Position {
-        self.positions.entry(coord).or_insert_with(|| {
-            let coord_in_grid = Coord {
-                x: coord.x.rem_euclid(self.grid.width()),
-                y: coord.y.rem_euclid(self.grid.height()),
-            };
-            let mut position_type = *self
-                .grid
-                .get(&coord_in_grid)
-                .expect(format!("no position at {:?}", coord_in_grid).as_str());
-            if position_type == PositionType::Start {
-                position_type = PositionType::Empty;
-            }
-            Position {
-                position_type,
-                explore_range: None,
-            }
-        })
-    }
+    fn get_type(&self, coord: Coord) -> PositionType {
+        let coord_in_grid = Coord {
+            x: coord.x.rem_euclid(self.grid.width()),
+            y: coord.y.rem_euclid(self.grid.height()),
+        };
+        let position_type = *self
+            .grid
+            .get(&coord_in_grid)
+            .unwrap_or_else(|| panic!("no position at {:?}", coord_in_grid));
 
-    fn count_explored(&self, turn: u32) -> usize {
-        let turn_mod = turn % 2;
-        self.positions.values()
-            .filter(|position| {
-                matches!(position.explore_range, Some(range) if range %2 == turn_mod && range <= turn)
-            })
-            .count()
+        if position_type == PositionType::Start {
+            PositionType::Empty
+        } else {
+            position_type
+        }
     }
 }
 
@@ -80,10 +71,7 @@ impl FromStr for InfiniteMap {
     type Err = CannotParseGrid;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        std::prelude::rust_2015::Ok(Self {
-            grid: s.parse()?,
-            positions: HashMap::new(),
-        })
+        std::prelude::rust_2015::Ok(Self { grid: s.parse()? })
     }
 }
 
@@ -92,19 +80,59 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_infinite_map() {
-        let mut map: InfiniteMap =
-            challenges_common::get_input_content(&["aoc", "2023", "21-test.txt"])
-                .parse()
-                .unwrap();
+    fn test_infinite_map_6() {
+        let content = challenges_common::get_input_content(&["aoc", "2023", "21-test.txt"]);
+        assert_eq!(run(&content, 6).unwrap(), 16);
+    }
 
-        map.explore(100).unwrap();
-        assert_eq!(map.count_explored(6), 16);
-        assert_eq!(map.count_explored(10), 50);
-        assert_eq!(map.count_explored(50), 1594);
-        assert_eq!(map.count_explored(100), 6536);
-        // assert_eq!(map.count_explored(500), 167004);
-        // assert_eq!(map.count_explored(1000), 668697);
-        // assert_eq!(map.count_explored(5000), 16733044);
+    #[test]
+    fn test_infinite_map_10() {
+        let content = challenges_common::get_input_content(&["aoc", "2023", "21-test.txt"]);
+        assert_eq!(run(&content, 10).unwrap(), 50);
+        // assert_eq!(run(&content, 50).unwrap(), 1594);
+        // assert_eq!(run(&content, 100).unwrap(), 6536);
+        // assert_eq!(run(&content, 500).unwrap(), 167004);
+        // assert_eq!(run(&content, 1000).unwrap(), 668697);
+        // assert_eq!(run(&content, 5000).unwrap(), 16733044);
+    }
+
+    #[test]
+    fn test_infinite_map_50() {
+        let content = challenges_common::get_input_content(&["aoc", "2023", "21-test.txt"]);
+        assert_eq!(run(&content, 50).unwrap(), 1594);
+        // assert_eq!(run(&content, 100).unwrap(), 6536);
+        // assert_eq!(run(&content, 500).unwrap(), 167004);
+        // assert_eq!(run(&content, 1000).unwrap(), 668697);
+        // assert_eq!(run(&content, 5000).unwrap(), 16733044);
+    }
+
+    #[test]
+    fn test_infinite_map_100() {
+        let content = challenges_common::get_input_content(&["aoc", "2023", "21-test.txt"]);
+        assert_eq!(run(&content, 100).unwrap(), 6536);
+        // assert_eq!(run(&content, 500).unwrap(), 167004);
+        // assert_eq!(run(&content, 1000).unwrap(), 668697);
+        // assert_eq!(run(&content, 5000).unwrap(), 16733044);
+    }
+
+    #[test]
+    fn test_infinite_map_500() {
+        let content = challenges_common::get_input_content(&["aoc", "2023", "21-test.txt"]);
+        assert_eq!(run(&content, 500).unwrap(), 167004);
+        // assert_eq!(run(&content, 1000).unwrap(), 668697);
+        // assert_eq!(run(&content, 5000).unwrap(), 16733044);
+    }
+
+    #[test]
+    fn test_infinite_map_1000() {
+        let content = challenges_common::get_input_content(&["aoc", "2023", "21-test.txt"]);
+        assert_eq!(run(&content, 1000).unwrap(), 668697);
+        // assert_eq!(run(&content, 5000).unwrap(), 16733044);
+    }
+
+    #[test]
+    fn test_infinite_map_5000() {
+        let content = challenges_common::get_input_content(&["aoc", "2023", "21-test.txt"]);
+        assert_eq!(run(&content, 5000).unwrap(), 16733044);
     }
 }
