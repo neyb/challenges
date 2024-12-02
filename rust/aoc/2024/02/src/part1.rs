be0a81934059719f5dac43092f9d70ec1047f853
neyb <noo.blaster@gmail.com>
2024-12-02 09:29:00 +0100

    aoc 2024 day 2

---
+++ rust/aoc/2024/02/src/part1.rs
@@ -0,0 +1,65 @@
+use anyhow::*;
+use itertools::Itertools;
+use std::str::FromStr;
+
+type Res = usize;
+pub(crate) fn run(content: &String) -> Result<Res> {
+    let lines: Vec<_> = content
+        .lines()
+        .map(|line| line.parse::<Line>())
+        .try_collect()?;
+
+    Ok(lines.iter().filter(|line| line.is_safe()).count())
+}
+
+struct Line {
+    numbers: Vec<isize>,
+}
+
+impl Line {
+    fn is_safe(&self) -> bool {
+        let mut sign = None;
+        self.numbers.windows(2).map(|w| w[1] - w[0]).all(|diff| {
+            diff.abs() <= 3
+                && diff.abs() > 0
+                && match sign {
+                    None => {
+                        sign = if diff > 0 {
+                            Some(Sign::Positive)
+                        } else {
+                            Some(Sign::Negative)
+                        };
+                        true
+                    }
+                    Some(Sign::Positive) => diff > 0,
+                    Some(Sign::Negative) => diff < 0,
+                }
+        })
+    }
+}
+
+impl FromStr for Line {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        Ok(Self {
+            numbers: s.split(" ").map(|s| s.parse()).try_collect()?,
+        })
+    }
+}
+
+enum Sign {
+    Positive,
+    Negative,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_run() {
+        let content = challenges_common::get_input_content(&["aoc", "2024", "02-test.txt"]);
+        assert_eq!(run(&content).unwrap(), 0);
+    }
+}
---
+++ rust/aoc/2024/02/src/part2.rs
@@ -0,0 +1,42 @@
+use crate::Line;
+use anyhow::*;
+use itertools::Itertools;
+use std::iter::once;
+
+type Res = usize;
+pub(crate) fn run(content: &String) -> Result<Res> {
+    let lines: Vec<Line> = content.lines().map(|line| line.parse()).try_collect()?;
+    Ok(lines.iter().filter(|line| line.is_safe_part2()).count())
+}
+
+trait Part2Line: Sized {
+    fn possible_inputs(&self) -> Vec<Self>;
+    fn is_safe_part2(&self) -> bool;
+}
+
+impl Part2Line for Line {
+    fn possible_inputs(&self) -> Vec<Self> {
+        once(self.clone())
+            .chain((0..self.numbers.len()).map(|i| {
+                let mut numbers = self.numbers.clone();
+                numbers.remove(i);
+                Self { numbers }
+            }))
+            .collect()
+    }
+
+    fn is_safe_part2(&self) -> bool {
+        self.possible_inputs().iter().any(|line| line.is_safe())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_run() {
+        let content = challenges_common::get_input_content(&["aoc", "2024", "02-test.txt"]);
+        assert_eq!(run(&content).unwrap(), 0);
+    }
+}
