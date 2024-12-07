0f502670aaace0a9bb1456b108cfb165973ddc89
neyb <noo.blaster@gmail.com>
2024-12-07 18:40:04 +0100

    factorize code

--- rust/aoc/2024/07/src/main.rs
+++ rust/aoc/2024/07/src/main.rs
@@ -4,18 +4,72 @@ use std::str::FromStr;
 
 fn main() {
     let content = challenges_common::get_input_content(&["aoc", "2024", "07.txt"]);
-    println!("part1: {:?}", part1::run(&content));
-    println!("part2: {:?}", part2::run(&content));
+    println!("part1: {:?}", run::<part1::Operator>(&content));
+    println!("part2: {:?}", run::<part2::Operator>(&content));
 }
 
 mod part1;
 mod part2;
 
-type Res = usize;
+type Res = u64;
+
+fn run<Op: Operator>(content: &str) -> anyhow::Result<Res> {
+    let equations: Equations = content.parse()?;
+    anyhow::Ok(equations.get_solvable_result_sum::<Op>())
+}
+
+struct Equations(Vec<Equation>);
+
+impl Equations {
+    fn get_solvable_result_sum<Op: Operator>(&self) -> Res {
+        self.0
+            .iter()
+            .filter(|eq| eq.can_be_solved::<Op>())
+            .map(|equation| equation.result)
+            .sum()
+    }
+}
 
 struct Equation {
     result: Res,
     operands: Vec<Res>,
+}
+
+impl Equation {
+    fn can_be_solved<Op: Operator>(&self) -> bool {
+        self.can_be_solved_with(&mut Vec::<Op>::new())
+    }
+
+    fn can_be_solved_with<Op: Operator>(&self, mut operators: &mut Vec<Op>) -> bool {
+        if self.operands.len() == operators.len() + 1 {
+            self.operands
+                .iter()
+                .tuple_windows()
+                .zip(operators)
+                .fold(None, |sum, ((a, b), op)| match sum {
+                    None => Some(op.apply(*a, *b)),
+                    Some(res) => Some(op.apply(res, *b)),
+                })
+                == Some(self.result)
+        } else {
+            for operator in Op::all() {
+                operators.push(operator);
+                if self.can_be_solved_with(&mut operators) {
+                    return true;
+                }
+                operators.pop();
+            }
+            false
+        }
+    }
+}
+
+impl FromStr for Equations {
+    type Err = Error;
+
+    fn from_str(s: &str) -> anyhow::Result<Self> {
+        Ok(Self(s.lines().map(|line| line.parse()).try_collect()?))
+    }
 }
 
 impl FromStr for Equation {
@@ -31,4 +85,9 @@ impl FromStr for Equation {
             operands: operands.split(" ").map(|op| op.parse()).try_collect()?,
         })
     }
+}
+
+trait Operator: Sized {
+    fn all() -> Vec<Self>;
+    fn apply(&self, a: Res, b: Res) -> Res;
 }
--- rust/aoc/2024/07/src/part2.rs
+++ rust/aoc/2024/07/src/part2.rs
@@ -1,66 +1,16 @@
-use crate::Equation;
-use anyhow::*;
-use itertools::Itertools;
+use crate::Res;
 
-type Res = usize;
-pub(crate) fn run(content: &str) -> Result<Res> {
-    let equations: Vec<Equation> = content.lines().map(|line| line.parse()).try_collect()?;
-
-    Ok(equations
-        .iter()
-        .filter(|eq| eq.can_be_solved())
-        .map(|equation| equation.result)
-        .sum())
-}
-
-trait Part2Equation {
-    fn can_be_solved(&self) -> bool;
-    fn can_be_solved_with(&self, operators: &mut Vec<Operation>) -> bool;
-}
-
-impl Part2Equation for Equation {
-    fn can_be_solved(&self) -> bool {
-        self.can_be_solved_with(&mut Vec::new())
-    }
-
-    fn can_be_solved_with(&self, mut operators: &mut Vec<Operation>) -> bool {
-        if self.operands.len() == operators.len() + 1 {
-            let result = self.operands.iter().tuple_windows().zip(operators).fold(
-                None,
-                |sum, ((a, b), op)| match sum {
-                    None => Some(op.apply(*a, *b)),
-                    Some(res) => Some(op.apply(res, *b)),
-                },
-            );
-            result == Some(self.result)
-        } else {
-            operators.push(Operation::Add);
-            if self.can_be_solved_with(&mut operators) {
-                return true;
-            }
-            operators.pop();
-            operators.push(Operation::Multiply);
-            if self.can_be_solved_with(&mut operators) {
-                return true;
-            }
-            operators.pop();
-            operators.push(Operation::Concatenate);
-            if self.can_be_solved_with(&mut operators) {
-                return true;
-            }
-            operators.pop();
-            false
-        }
-    }
-}
-
-enum Operation {
+pub enum Operator {
     Add,
     Multiply,
     Concatenate,
 }
 
-impl Operation {
+impl crate::Operator for Operator {
+    fn all() -> Vec<Self> {
+        vec![Self::Add, Self::Multiply, Self::Concatenate]
+    }
+
     fn apply(&self, a: Res, b: Res) -> Res {
         match self {
             Self::Add => a + b,
@@ -81,17 +31,18 @@ impl Operation {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use anyhow::*;
 
     #[test]
     fn basic_concat_test() -> Result<()> {
-        let equation: Equation = "156: 15 6".parse()?;
-        assert_eq!(equation.can_be_solved(), true);
+        let equation: crate::Equation = "156: 15 6".parse()?;
+        assert_eq!(equation.can_be_solved::<Operator>(), true);
         Ok(())
     }
 
     #[test]
     fn test_run() {
         let content = challenges_common::get_input_content(&["aoc", "2024", "07-test.txt"]);
-        assert_eq!(run(&content).unwrap(), 11387);
+        assert_eq!(crate::run::<Operator>(&content).unwrap(), 11387);
     }
 }
