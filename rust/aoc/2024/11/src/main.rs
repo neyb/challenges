4954e91273fa75eb1db73d7a18b578331e9024ac
neyb <noo.blaster@gmail.com>
2024-12-11 16:39:10 +0100

    factorize code

--- rust/aoc/2024/11/src/main.rs
+++ rust/aoc/2024/11/src/main.rs
@@ -1,3 +1,9 @@
+use anyhow::Error;
+use itertools::Itertools;
+use std::collections::HashMap;
+use std::ops::Deref;
+use std::str::FromStr;
+
 fn main() {
     let content = challenges_common::get_input_content(&["aoc", "2024", "11.txt"]);
     println!("part1: {:?}", part1::run(&content));
@@ -6,3 +12,79 @@ fn main() {
 
 mod part1;
 mod part2;
+
+struct StoneLine {
+    current_stones: HashMap<Stone, usize>,
+}
+
+impl StoneLine {
+    fn blink(&mut self) {
+        let stones = self
+            .current_stones
+            .iter()
+            // .copied()
+            .map(|(&stone, &count)| (stone, count))
+            .collect_vec();
+        for (stone, count) in stones {
+            let (first, second) = stone.blink();
+            *self.current_stones.entry(stone).or_insert(0) -= count;
+            *self.current_stones.entry(first).or_insert(0) += count;
+            if let Some(second_stone) = second {
+                *self.current_stones.entry(second_stone).or_insert(0) += count;
+            }
+        }
+    }
+
+    fn len(&self) -> TStone {
+        self.current_stones
+            .iter()
+            .map(|(stone, count)| *count as TStone)
+            .sum()
+    }
+}
+
+type TStone = u64;
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
+struct Stone(TStone);
+
+impl Stone {
+    fn blink(&self) -> (Self, Option<Self>) {
+        match self.0 {
+            0 => (Stone(1), None),
+            _ if (self.0.ilog10() + 1) % 2 == 0 => {
+                let mask = (10 as TStone).pow((self.0.ilog10() + 1) / 2);
+                (Stone(self.0 / mask), Some(Stone(self.0 % mask)))
+            }
+            _ => (Stone(self.0 * 2024), None),
+        }
+    }
+}
+
+impl From<TStone> for Stone {
+    fn from(value: TStone) -> Self {
+        Self(value)
+    }
+}
+
+impl Deref for Stone {
+    type Target = TStone;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl FromStr for StoneLine {
+    type Err = Error;
+
+    fn from_str(s: &str) -> anyhow::Result<Self> {
+        let stones: Vec<Stone> = s
+            .split(" ")
+            .map(|s| s.parse().map(|n| Stone(n)))
+            .try_collect()?;
+
+        anyhow::Ok(Self {
+            current_stones: stones.into_iter().counts(),
+        })
+    }
+}
--- rust/aoc/2024/11/src/part1.rs
+++ rust/aoc/2024/11/src/part1.rs
@@ -1,48 +1,14 @@
+use crate::{StoneLine, TStone};
 use anyhow::*;
-use itertools::Itertools;
-use std::str::FromStr;
 
-type Res = usize;
+type Res = TStone;
 pub(crate) fn run(content: &str) -> Result<Res> {
     let mut stone_line: StoneLine = content.parse()?;
     for _ in 0..25 {
         stone_line.blink();
     }
 
-    Ok(stone_line.stones.len())
-}
-
-type Stone = u64;
-
-struct StoneLine {
-    stones: Vec<Stone>,
-}
-
-impl StoneLine {
-    fn blink(&mut self) {
-        for i in (0..self.stones.len()).rev() {
-            let mut stone = &mut self.stones[i];
-            match stone {
-                0 => *stone = 1,
-                _ if (stone.ilog10() + 1) % 2 == 0 => {
-                    let mask = (10 as Stone).pow((stone.ilog10() + 1) / 2);
-                    let next_stone = *stone % mask;
-                    *stone /= mask;
-                    self.stones.insert(i + 1, next_stone)
-                }
-                _ => *stone *= 2024,
-            }
-        }
-    }
-}
-
-impl FromStr for StoneLine {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self> {
-        let stones = s.split(" ").map(|s| s.parse()).try_collect()?;
-        Ok(Self { stones })
-    }
+    Ok(stone_line.len())
 }
 
 #[cfg(test)]
